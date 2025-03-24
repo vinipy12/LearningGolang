bce2fd12465d64b98bd320b816a965bf804cc2d9
Vini <vinicius.loureiro.cavalcante12@gmail.com>
2025-03-23 22:06:49 -0400

    type casting.

--- GoClass/basic_types.go
+++ GoClass/basic_types.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	a := 2
-	b := 2.1
+	b := 3.1
 
 	fmt.Printf("a: %8T %[1]v\n", a)
+	fmt.Printf("b: %8T %[1]v\n", b)
+
+	a = int(b)
+	fmt.Printf("a: %8T %[1]v\n", a)
+	b = float64(a)
 	fmt.Printf("b: %8T %[1]v\n", b)
 }
